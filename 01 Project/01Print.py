2323286bd551fc17375e60ab25a055914153bc19
Wryn Wagner <wagner.wryn@gmail.com>
2018-08-11 11:31:53 -0600

    Started Magic Numbers Project

--- 01 Project/01Print.py
+++
@@ -1,3 +0,0 @@
-age = int(input('Enter your age: '))
-age_seconds = age *365*24*60*60
-print("You have lived for {seconds} seconds. This corresponds to {years} years.".format(seconds=age_seconds,years=age))
